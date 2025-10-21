e9a4161e5cbd59f71894728be0e69d90880be8fb
dimit <dimitriisilva.9@gmail.com>
2025-10-21 01:42:19 -0300

    up

--- Main.java
+++ Main.java
@@ -1,12 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        Produto produto1= new Produto("Geladeira",1900);
+        Produto produto1 = new Produto("Geladeira", 1900);
+        Produto produto2 = new Produto("Smartphone", 3200);
 
-produto1.setQntEmEstoque(10);
-
+        produto1.setQntEmEstoque(2);
         System.out.println(produto1.toString());
+        System.out.println("------------------------------------------------------------------");
 
+        Eletronico eletronico = new Eletronico("TV", 2299.0, 18);
+        eletronico.setQntEmEstoque(2);
+        System.out.println(eletronico.toString());
+        System.out.println("-------------------------------------");
+
+        Produto produto3 = new Eletronico("Cama", 1399.99, 12);
+        produto3.exibirDetalhes();
     }
 }
--- Produto.java
+++ Produto.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Produto {
     Scanner sc = new Scanner(System.in);
     private String nome;
-    private int preco;
+    private double preco;
     private int qntEmEstoque;
 
-    public Produto( String nome, int preco){
-        this.nome=nome;
-        this.preco=preco;
-        qntEmEstoque=0;
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+        qntEmEstoque = 0;
     }
 
-    public int getPreco() {
+    public double getPreco() {
         return preco;
     }
 
@@ -25,18 +25,23 @@ public class Produto {
     }
 
     public void setQntEmEstoque(int qntEmEstoque1) {
-        while (qntEmEstoque1<=0){
-            System.out.println("Digite uma quantidade maior que 0!");
-             qntEmEstoque1=sc.nextInt();
+        while (qntEmEstoque1 < 0) {
+            System.out.println("Digite uma quantidade válida!");
+            qntEmEstoque1 = sc.nextInt();
         }
         this.qntEmEstoque = qntEmEstoque1;
     }
 
     @Override
     public String toString() {
-       return  "nome: " + nome + "\n" +
-        "preco: " + preco  + "\n" +
-        "Quantidade em estoque: " + qntEmEstoque;
+        return "nome: " + nome + "\n" +
+                "preco: $" + String.format("%.2f\n",preco) +
+                "Quantidade em estoque: " + qntEmEstoque;
+
+    }
+    public void exibirDetalhes(){
+        System.out.printf("nome: %s\n" , nome);
+        System.out.println("preco: " + preco);
 
     }
 }
